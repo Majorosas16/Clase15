f46494cbe678371b08b053f6969e5dc1c426e899
Majorosas16 <158408316+Majorosas16@users.noreply.github.com>
2024-03-19 08:07:18 -0500

    Add files via upload

---
+++ Clase 15/script.js
@@ -0,0 +1,37 @@
+const contenedor=document.querySelector("#contenedor");
+
+const textoUno=document.createElement("p");
+textoUno.innerHTML="Hola este es mi primer párrafo";
+textoUno.classList.add("texto--rojo");
+textoUno.setAttribute("id","parrafoUno");
+
+const textoDos=document.createElement("p");
+textoDos.innerHTML="Hola este es mi segundo párrafo";
+textoDos.classList.add("texto--verde");
+textoDos.setAttribute("style","font-size:3rem");
+
+contenedor.appendChild(textoUno);
+contenedor.appendChild(textoDos);
+
+const digimones = [
+    "https://digimon.shadowsmith.com/img/koromon.jpg",
+    "https://digimon.shadowsmith.com/img/tsunomon.jpg",
+    "https://digimon.shadowsmith.com/img/yokomon.jpg",
+    "https://digimon.shadowsmith.com/img/motimon.jpg",
+    "https://digimon.shadowsmith.com/img/patamon.jpg",
+    "https://digimon.shadowsmith.com/img/kuwagamon.jpg",
+    "https://digimon.shadowsmith.com/img/greymon.jpg",
+    "https://digimon.shadowsmith.com/img/tanemon.jpg",
+    ];
+
+const digimonesContenedor = document.querySelector("#digimon");
+
+for (const digimon of digimones){
+    const img = document.createElement("img");
+    img.setAttribute("src", digimon);
+    img.setAttribute("alt", digimon);
+
+    img.classList.add("img");
+
+    digimonesContenedor.appendChild(img);
+}
